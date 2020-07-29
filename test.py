931123b81803ed85f7fdcdaa41fba19cea64d939
Tyler Casper <casperw@colorado.edu>
2020-07-28 21:46:45 -0500

    Still working on algorithm, wanted to update before signing off.

--- test.py
+++ test.py
@@ -20,15 +20,26 @@ grid_locs = [[(x*50 + X_OFFSET, y*50 + Y_OFFSET) for x in range(w)] for y in ran
 SUDOKU_INPUT = "040509108710004500030007940965070231070000080103960000000050700300006000650801300"
 s = list(SUDOKU_INPUT)
 
+print(s)
+
 all_ = ['1','2','3','4','5','6','7','8','9']
 
 
 for i, num in enumerate(s):
     if num == "0": # we need to implement an algorithm to fill in numbers, here
-        k = floor(i / 9) * 9
-        verts = [z for z in s[i::9] if z not in s[k:k+9]]
-        maybe = [z for z in all_ if z not in (verts + s[k:k+9])]
+        k = floor(i / 9) * 9 # the index of the beginning of the row that the i index is in
+        verts = [z for z in s[i::9] if z not in s[k:k+9]] # the set of numbers vertically that are not in the horizontal subspace
+        maybe = [z for z in all_ if z not in (verts + s[k:k+9])] # the subspace of numbers that can be possible solutions
         print(i, k, verts, maybe)
+
+        # if len(maybe) == 1:
+        #     s[i] = maybe
+        #     print(i, s)
+        #     break
+        # else:
+        #     for l, num in enumerate(maybe):
+        #         s[i] = maybe[l]
+
 
 
 print(s, "\n", s[0:9], "\n", s[0::9])
